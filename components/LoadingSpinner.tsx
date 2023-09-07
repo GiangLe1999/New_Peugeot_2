9c1e02093d9d3f4dd3a14c41d30af7774d8aaa95
GiangLe1999 <legiangbmt09@gmail.com>
2023-09-07 10:36:31 +0700

    Done introduce page

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 const LoadingSpinner: FC<Props> = (props): JSX.Element => {
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center text-primary">
+    <div className="w-full h-[300px] flex flex-col items-center justify-center text-primary">
       <ImSpinner3 className="animate-spin" size={50} />
       <p className="mt-2 text-xl">Đang tải dữ liệu</p>
     </div>
