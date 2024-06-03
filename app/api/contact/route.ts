526825080dceca560b52084295df8b189d73502c
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-06-03 15:40:31 +0700

    Fix contact form error

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,44 +3,50 @@ import Contact from "@/model/Contact";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { name, phone, email, carName, carLine, content, section, service } =
-    await req.json();
+  try {
+    const { name, phone, email, carName, carLine, content, section, service } =
+      await req.json();
 
-  if (
-    !name ||
-    !name.trim() ||
-    !phone ||
-    !phone.trim() ||
-    !email ||
-    !email.trim() ||
-    !carName ||
-    !carLine ||
-    !section ||
-    !service
-  )
-    return NextResponse.json(
-      { error: "Invalid request body!" },
-      {
-        status: 422,
-      }
-    );
+    if (
+      !name ||
+      !name.trim() ||
+      !phone ||
+      !phone.trim() ||
+      !email ||
+      !email.trim() ||
+      !carName ||
+      !carLine ||
+      !section ||
+      !service
+    )
+      return NextResponse.json(
+        { error: "Invalid request body!" },
+        {
+          status: 422,
+        }
+      );
 
-  await dbConnect();
+    await dbConnect();
 
-  const contact = new Contact({
-    name,
-    phone,
-    email,
-    carName,
-    carLine,
-    content,
-    section,
-    service,
-  });
+    const contact = new Contact({
+      name,
+      phone,
+      email,
+      carName,
+      carLine,
+      content,
+      province: section,
+      service,
+    });
 
-  await contact.save();
+    await contact.save();
 
-  return NextResponse.json(contact, {
-    status: 201,
-  });
+    return NextResponse.json(contact, {
+      status: 201,
+    });
+  } catch (error) {
+    return NextResponse.json(error, {
+      status: 500,
+    });
+  }
 }
