0d06ff3f63e3eeebc44119616a6e942cad02bead
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-06-09 09:17:20 +0700

    Remove unused script tag

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -6,10 +6,6 @@ interface Props {}
 const GoogleAnalytics: FC<Props> = (props): JSX.Element => {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-7CN8KFXGLW"
-      ></Script>
       <Script id="google-analytic">
         {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
