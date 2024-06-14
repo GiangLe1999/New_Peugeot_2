1d8e2caac4d5791bd6cdf548803baf2b53f78607
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-06-14 22:20:37 +0700

    Fix image error at single car page

--- components/carPage/SalerCard.tsx
+++ components/carPage/SalerCard.tsx
@@ -13,7 +13,7 @@ const SalerCard: FC<Props> = (props): JSX.Element => {
       <div className="bg-secondary rounded-md overflow-hidden">
         <div className="w-full aspect-square relative">
           <NextImage
-            src="/images/gioi-thieu/phong-kinh-doanh-mazda-sai-gon.png"
+            src="/images/gioi-thieu/phong-kinh-doanh-peugeot-thu-duc.webp"
             alt="Phòng kinh doanh Mazda"
           />
         </div>
