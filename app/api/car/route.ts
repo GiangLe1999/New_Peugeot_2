bac6cd0d1cc7c6fbd3199c13a7b7b82dc9670efd
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-06-16 08:20:04 +0700

    Update route.ts

--- app/api/car/route.ts
+++ app/api/car/route.ts
@@ -2,6 +2,8 @@ import dbConnect from "@/lib/db";
 import Car from "@/model/Car2";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 
 export async function GET(req: Request) {
   try {
